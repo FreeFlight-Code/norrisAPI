64c7c507997cf7d6aff9005bc72a580a7d22be08
David Fischer <davidrfischer77@gmail.com>
2020-03-08 06:50:01 -0600

    universal and button styles added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Chuck Norris Rules!!!
+      <div class="text-red">This will be red text.</div>
+      <button className="blue">MORE INFO HEre</button>
+      <button className="red">warning</button>
+      <button className="green">Success</button>
+      <button>default</button>
+    </div>
+  );
+}
+
+export default App;
