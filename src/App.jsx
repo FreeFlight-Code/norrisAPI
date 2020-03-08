127c4aca350e0451f3cdf7d58fe14d399615f375
David Fischer <davidrfischer77@gmail.com>
2020-03-08 08:10:58 -0600

    nav header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Header from './components/Header'
 import Home from './pages/Home'
 import Categories from './pages/Categories'
 import Jokes from './pages/Jokes'
@@ -11,25 +12,32 @@ export default class App extends React.Component {
 
   render() { 
     return (
-      <Router>
-        <Switch>
-          <Route path="/categories">
-            <Categories />
-          </Route>
-          <Route path="/jokes">
-            <Jokes />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/">
-            <div>404 Page</div>
-          </Route>
-        </Switch>
-      </Router>
+
+      <div className='ChuckNorrisApp'>
+
+        <Header />
+
+        <Router>
+          <Switch>
+            <Route path="/categories">
+              <Categories />
+            </Route>
+            <Route path="/jokes">
+              <Jokes />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/">
+              <div>404 Page</div>
+            </Route>
+          </Switch>
+        </Router>
+
+      </div>
     );
   }
 }
