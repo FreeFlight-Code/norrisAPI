2e82d772c389c548eb3e4878fcb322f47f680d19
David Fischer <davidrfischer77@gmail.com>
2020-03-09 06:31:50 -0600

    refactored form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Form from '../components/Form';
 
-class Home extends Component {
+class Home extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {  }
+
 	}
+
 	render() { 
-		return ( 
-			<div>Home Page</div>
-		 );
+
+		console.log(this.state)
+		return (
+      <div>
+        Home Page
+		<Form/>
+      </div>
+    );
 	}
 }
  
