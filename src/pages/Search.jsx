2aaedf33539f62f517532369034fa38768c94573
David Fischer <davidrfischer77@gmail.com>
2020-03-26 15:37:34 -0600

    added search functionality

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,10 +4,63 @@ import React, { Component } from "react";
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchTerm: "",
+      jokes: []
+    };
   }
+
+  handleinputChange(val){
+    console.log(val)
+    this.setState({
+      searchTerm: val
+    })
+  }
+
+  handleSubmit(){
+    const {searchTerm} = this.state;
+    fetch(`https://api.chucknorris.io/jokes/search?query=${searchTerm}`)
+      .then(res => res.json())
+      .then(obj => {
+        let jokesArray = obj.result;
+        console.log(jokesArray)
+        this.setState({ 
+          searchTerm: "",
+          jokes: jokesArray
+        })
+      });
+  }
+
+  truncJoke(string, charLimit){
+    return string.slice(0, charLimit + 1) + "..."
+  }
+
+  renderResults(){
+    const {jokes} = this.state;
+    console.log(jokes)
+    if(jokes && jokes.length > 0){
+      return jokes.map((joke, i)=>{
+        joke = this.truncJoke(joke.value, 50);
+        return(
+        <div key={`joke-${i}`}>{joke}</div>
+        )
+      })
+    }
+  }
+
   render() {
-    return <div className="page search">Search page</div>;
+    console.log(this.state.jokes)
+    return (
+      <div className="page search">
+        <label htmlFor="searchBar">Search Bar</label>
+        <input value={this.state.searchTerm} id="searchBar" type="text" onChange={e=>{this.handleinputChange(e.target.value)}} placeholder="Search Chuck Norris Joke by Key Word"></input>
+        <button className="blue" onClick={e=>{this.handleSubmit()}}>Submit</button>
+        <div className="results">
+          <h3>Results</h3>
+          {this.renderResults()}
+        </div>
+      </div>
+    )
   }
 }
 
