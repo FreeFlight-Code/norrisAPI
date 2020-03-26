ad32ad968178b269b049b548dace3ef02721d50c
David Fischer <davidrfischer77@gmail.com>
2020-03-26 15:37:21 -0600

    added categories functionality

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,14 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Categories extends Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {  }
-	}
-	render() { 
-		return <div className="page categories">Categories page</div>;
-	}
+class Categories extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentJoke: "No Current Joke Selected.",
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.chucknorris.io/jokes/categories")
+      .then(res => res.json())
+      .then(categories => this.setState({ categories: categories }));
+  }
+
+  categoriesList() {
+    const { categories } = this.state;
+    return categories.map((category, i) => {
+      return (
+        <div
+          className="category"
+          onClick={e => {
+            this.getJoke(category);
+          }}
+          key={`category-${i}`}
+        >
+          {category}
+        </div>
+      );
+    });
+  }
+  getJoke(category) {
+    if (category) {
+      fetch(
+        `https://api.chucknorris.io/jokes/random?category=${category}`
+      )
+        .then(res => res.json())
+        .then(joke => {
+          console.log(joke);
+          this.setState({ currentJoke: joke.value });
+        });
+    }
+    return this.state.currentJoke;
+  }
+
+  render() {
+    return (
+      <div className="page categories">
+        <div className="categoryList">{this.categoriesList()}</div>
+        {this.getJoke()}
+      </div>
+    );
+  }
 }
- 
+
 export default Categories;
