756e5ddc5e6fb44d0776d57edb04919dc2f852de
David Fischer <davidrfischer77@gmail.com>
2020-03-26 15:39:24 -0600

    styling - added png to index.html and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,26 @@ import React from 'react';
 class Header extends React.Component {
 
 	render() { 
-		const {loggedin} = this.props;
+		// const {loggedin} = this.props;
 		return (
-			<header>
-				<nav>
-					<span className="loggedin">{loggedin ? "Logged in" : "Not logged in"}</span>
-					<a href="/"><span>Home</span></a>
-					<a href="jokes"><span>Jokes</span></a>
-					<a href="categories"><span>Categories</span></a>
-					<a href="search"><span>Search</span></a>
-				</nav>
-			</header>
+      <header>
+        <nav>
+          <a href="/">
+            <span>Home</span>
+          </a>
+          <a href="jokes">
+            <span>Jokes</span>
+          </a>
+          <a href="categories">
+            <span>Categories</span>
+          </a>
+          <a href="search">
+            <span>Search</span>
+          </a>
+          {/* <span className="loggedin">{!loggedin ? "Log in" : "Log out"}</span> */}
+          <img style={{width: "50px"}} src="https://assets.chucknorris.host/img/avatar/chuck-norris.png"/>
+        </nav>
+      </header>
     );
 	}
 }
