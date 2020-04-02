9a7df5ee78455a436859777424951a9f9a280b3e
David Fischer <davidrfischer77@gmail.com>
2020-04-01 18:08:20 -0600

    fixed regex

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -12,30 +12,30 @@ class LoginForm extends React.Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.togglePasswordvisibility = this.togglePasswordvisibility.bind(this);
     // this.validateEmailAndPassword = this.validateEmailAndPassword.bind(this);
-    document.addEventListener("keydown", this.validateEmailAndPassword);
+    document.addEventListener("keyup", this.validateEmailAndPassword);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keypress", this.validateEmailAndPassword);
+    document.removeEventListener("keyup", this.validateEmailAndPassword);
   }
 
   validateEmailAndPassword = () => {
-	//   const emailRegex = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$");
-	//   const passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{6,10}$");
-	  const emailRegex = new RegExp("asd");
-	  const passwordRegex = new RegExp("asd");
-		  const { email, password } = this.state;
-		  const submitButton = document.querySelector("button.submit");
-		  if (emailRegex.test(email) && passwordRegex.test(password)) {
-			if (submitButton.className.indexOf("disabled") > 0) {
-			  submitButton.classList.remove("disabled");
-			}
-		  } else {
-			if (submitButton.className.indexOf("disabled") < 0) {
-			  submitButton.classList.add("disabled");
-			}
-		  }
-  }
+    const emailRegex = new RegExp(
+      "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+    );
+    const passwordRegex = new RegExp("^[a-zA-Z0-9]{6,10}$");
+    const { email, password } = this.state;
+    const submitButton = document.querySelector("button.submit");
+    if (emailRegex.test(email) && passwordRegex.test(password)) {
+      if (submitButton.className.indexOf("disabled") > 0) {
+        submitButton.classList.remove("disabled");
+      }
+    } else {
+      if (submitButton.className.indexOf("disabled") < 0) {
+        submitButton.classList.add("disabled");
+      }
+    }
+  };
 
   togglePasswordvisibility() {
     var x = document.getElementById("password");
@@ -49,33 +49,33 @@ class LoginForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const { email, password } = this.state;
-	this.props.dispatch({ type: "DISPLAY_MODAL" });
-	this.props.dispatch({ type: "LOG_IN", password: password, email: email });
-	this.setState({
-	email: "",
-	password: ""
-	});
-	setTimeout(() => {
-	this.props.dispatch({ type: "REMOVE_MODAL" });
-	}, 3000);
+    this.props.dispatch({ type: "DISPLAY_MODAL" });
+    this.props.dispatch({ type: "LOG_IN", password: password, email: email });
+    this.setState({
+      email: "",
+      password: ""
+    });
+    setTimeout(() => {
+      this.props.dispatch({ type: "REMOVE_MODAL" });
+    }, 3000);
   }
 
-  handleInputChange({type, value}) {
-	  this.setState({
-		  [type]: value
-		});
-	}
-	
-	render() {
-		const { email, password } = this.state;
-		console.log(this.state)
+  handleInputChange({ type, value }) {
+    this.setState({
+      [type]: value
+    });
+  }
+
+  render() {
+    const { email, password } = this.state;
+    console.log(this.state);
 
     return (
       <form className="loginForm">
         <label>Email</label>
         <input
           onChange={e => this.handleInputChange(e.target)}
-          // pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+        //   pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}"
           type="email"
           name="email"
           id="email"
